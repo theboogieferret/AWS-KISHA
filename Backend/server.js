ed00ffd24a478b017f0ba0406bbdc759a4ce702a
guy cohen <guycohen@live.com>
2026-01-10 23:34:48 +0200

    db5

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const oracledb = require("oracledb");
 const common = require("oci-common");
 const secrets = require("oci-secrets");
+const cors = require("cors")
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
